e4f4d272d47852fcdabab19917c814b311f68751
hoskinson <64093276+hoskinson@users.noreply.github.com>
2020-04-21 19:58:38 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+    sumOfTwoNumbers = firstNumber + secondNumber;
+    cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers;
+
+    return 0;
+}
