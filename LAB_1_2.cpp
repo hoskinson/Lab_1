210b66d1ce67f766ed3c80c046b099a0b49a4b6b
hoskinson <64093276+hoskinson@users.noreply.github.com>
2020-04-22 16:55:16 +0300

    Add files via upload

---
+++ LAB_1_2.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	setlocale(LC_ALL, "Russian");
+	cout << "\nВведите первое число : ";
+	cin >> a;
+	cout << "\nВведите второе число : ";
+	cin >> b;
+	c = a + b;
+	cout << "\nсумма : " << c;
+
+
+	return 0;
+}
+
